9811f1e10c316d2816771d90ebd5d8be40f5c791
David Mato <dmatochoya@gmail.com>
2020-12-06 12:54:43 +0100

    deleting folder

--- client/src/redux/reducers/recipeCategoriesReducer.tsx
+++
@@ -1,9 +0,0 @@
-export default function recipeCategoriesReducer(state = {}, action
-  : { type: string; categories: object; }) {
-  switch (action.type) {
-    case 'GET_RECIPE_CATEGORIES':
-      return action.categories;
-    default:
-      return state;
-  }
-}
--- client/src/redux/reducers/recipesReducer.tsx
+++
@@ -1,8 +0,0 @@
-export default function recipesReducer(state = [], action: { type: String; recipe: Object; }) {
-  switch (action.type) {
-    case 'GET_RECIPE':
-      return [...state, action.recipe.meals[0]];
-    default:
-      return state;
-  }
-}
