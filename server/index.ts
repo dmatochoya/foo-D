2ad43132a757ebebe29464572978cc56ad1986b7
David Mato <dmatochoya@gmail.com>
2021-01-10 16:01:53 +0100

    setting useFindAndModify to false

--- server/index.ts
+++ server/index.ts
@@ -15,7 +15,7 @@ app.use(cors());
 const port = process.env.PORT || 2000;
 const dbURL = process.env.DATABASEURL || 'mongodb://localhost/fooddb';
 
-connect(dbURL, { useNewUrlParser: true, useUnifiedTopology: true });
+connect(dbURL, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false });
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
